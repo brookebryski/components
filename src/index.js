ec6ab3443a2c33f8dc0910cf6f0227289a87786a
github username <github email address>
2021-05-03 10:23:27 -0500

    added first ui form for one comment

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+     <div className="ui container comments">
+         <div className="comment">
+             <a href="/" className="avatar">
+                 <img alt="avatar" />
+             </a>
+             <div className="content">
+                 <a href="/" className="author">
+                     Sam
+                 </a>
+                 <div className="metadata">
+                     <span className="date">Today at 6:00pm</span>
+                 </div>
+                 <div className="text">Nice blog post!</div>
+             </div>
+         </div>
+     </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-
-
-
-
-//<link
- // rel="stylesheet"
- // href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
-///>
