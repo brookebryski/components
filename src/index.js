243cfaf55a0ac8a9057ef664a0c213ae7836cb25
github username <github email address>
2021-05-03 12:18:21 -0500

    added props for avatar and comment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
     <div className="ui container comments">
-    <CommentDetail author="Sam" />
-    <CommentDetail author="Alex" />
-    <CommentDetail author="Jane" />
+    <CommentDetail 
+    author="Sam" 
+    timeAgo="Today at 4:45PM" 
+    text="Great post!"
+    avatar={faker.image.avatar()}
+    />
+    <CommentDetail 
+    author="Alex" 
+    timeAgo="Today at 2:00AM" 
+    text="Nice blog post!"
+    avatar={faker.image.avatar()}
+    />
+    <CommentDetail 
+    author="Jane" 
+    timeAgo="Today at 5:00PM" 
+    text="Loved this!"
+    avatar={faker.image.avatar()}
+    />
     </div>
     );
 };
